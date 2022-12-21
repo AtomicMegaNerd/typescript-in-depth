f3fb17b4d30c8c38c1ffa86a3665328adf88c36d
Chris Dunphy <chris@megaparsec.ca>
2022-12-21 12:58:38 -0700

    More work on the course

--- app.ts
+++ app.ts
@@ -1,3 +1,102 @@
-let greeting: string = 'Hello TypeScript in watch mode!';
+enum Category {
+  Biography,
+  Poetry,
+  Fiction,
+  Productivity,
+  History,
+  Sports,
+  Children,
+  Technology,
+}
 
-console.log(greeting);
+interface Book {
+  id: number;
+  title: string;
+  author: string;
+  category: Category;
+  available: boolean;
+}
+
+function GetAllBooks(): Book[] {
+  const books: Book[] = [
+    {
+      id: 1,
+      title: "Practical Haskell",
+      author: "Fred Smith",
+      category: Category.Technology,
+      available: false,
+    },
+    {
+      id: 2,
+      title: "NixOS for Dummies!",
+      author: "Alexandra Thompson",
+      category: Category.Technology,
+      available: true,
+    },
+    {
+      id: 3,
+      title: "Systems for Life",
+      author: "Chris Dunphy",
+      category: Category.Productivity,
+      available: false,
+    },
+    {
+      id: 4,
+      title: "Systems for Fun",
+      author: "Chris Dunphy",
+      category: Category.Productivity,
+      available: false,
+    },
+  ];
+  return books;
+}
+
+function BookToString(book: Book): string {
+  const category: string = Category[book.category];
+  return (
+    `Title: ${book.title}, Author: ${book.author}, Category: ${category}, ` +
+    `Available: ${book.available}`
+  );
+}
+
+function GetBookTitlesByCategory(categeryFilter: Category): Book[] {
+  console.log(`Getting books in category ${Category[categeryFilter]}`);
+  return GetAllBooks().filter((b) => b.category === categeryFilter);
+}
+
+function LogFirstAvailable(books: Book[]): Book {
+  const numberOfBooks: number = books.length;
+  const firstAvailable: Book = books.filter((b) => b.available)[0];
+  console.log(`Total Books: ${numberOfBooks}`);
+  console.log(`First Available => ${BookToString(firstAvailable)}`);
+  return firstAvailable;
+}
+
+function GetAllBookByID(id: number): Book {
+  return GetAllBooks().filter((b) => b.id === id)[0];
+}
+
+function CreateCustomerID(name: string, id: number): string {
+  return name + id;
+}
+
+// *************************************************************************
+
+let x: number;
+x = 5;
+
+let IdGenerator: (chars: string, nums: number) => string;
+IdGenerator = CreateCustomerID;
+
+let myId: string = IdGenerator("chris", 4001);
+console.log(myId);
+
+const allBooks: Book[] = GetAllBooks();
+LogFirstAvailable(allBooks);
+
+const productivityBooks: Book[] = GetBookTitlesByCategory(
+  Category.Productivity
+);
+productivityBooks.map((val) => console.log(val.id + " = " + val.title));
+
+console.log(GetAllBookByID(1).title);
--- js/app.js
+++ js/app.js
@@ -1,2 +1,78 @@
-let greeting = 'Hello TypeScript in watch mode!';
-console.log(greeting);
+var Category;
+(function (Category) {
+    Category[Category["Biography"] = 0] = "Biography";
+    Category[Category["Poetry"] = 1] = "Poetry";
+    Category[Category["Fiction"] = 2] = "Fiction";
+    Category[Category["Productivity"] = 3] = "Productivity";
+    Category[Category["History"] = 4] = "History";
+    Category[Category["Sports"] = 5] = "Sports";
+    Category[Category["Children"] = 6] = "Children";
+    Category[Category["Technology"] = 7] = "Technology";
+})(Category || (Category = {}));
+function GetAllBooks() {
+    const books = [
+        {
+            id: 1,
+            title: "Practical Haskell",
+            author: "Fred Smith",
+            category: Category.Technology,
+            available: false,
+        },
+        {
+            id: 2,
+            title: "NixOS for Dummies!",
+            author: "Alexandra Thompson",
+            category: Category.Technology,
+            available: true,
+        },
+        {
+            id: 3,
+            title: "Systems for Life",
+            author: "Chris Dunphy",
+            category: Category.Productivity,
+            available: false,
+        },
+        {
+            id: 4,
+            title: "Systems for Fun",
+            author: "Chris Dunphy",
+            category: Category.Productivity,
+            available: false,
+        },
+    ];
+    return books;
+}
+function BookToString(book) {
+    const category = Category[book.category];
+    return (`Title: ${book.title}, Author: ${book.author}, Category: ${category}, ` +
+        `Available: ${book.available}`);
+}
+function GetBookTitlesByCategory(categeryFilter) {
+    console.log(`Getting books in category ${Category[categeryFilter]}`);
+    return GetAllBooks().filter((b) => b.category === categeryFilter);
+}
+function LogFirstAvailable(books) {
+    const numberOfBooks = books.length;
+    const firstAvailable = books.filter((b) => b.available)[0];
+    console.log(`Total Books: ${numberOfBooks}`);
+    console.log(`First Available => ${BookToString(firstAvailable)}`);
+    return firstAvailable;
+}
+function GetAllBookByID(id) {
+    return GetAllBooks().filter((b) => b.id === id)[0];
+}
+function CreateCustomerID(name, id) {
+    return name + id;
+}
+// *************************************************************************
+let x;
+x = 5;
+let IdGenerator;
+IdGenerator = CreateCustomerID;
+let myId = IdGenerator("chris", 4001);
+console.log(myId);
+const allBooks = GetAllBooks();
+LogFirstAvailable(allBooks);
+const productivityBooks = GetBookTitlesByCategory(Category.Productivity);
+productivityBooks.map((val) => console.log(val.id + " = " + val.title));
+console.log(GetAllBookByID(1).title);
