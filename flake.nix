191f2263d1833c78dbd4a701c3bc8bcf0192d34e
Chris Dunphy <chris@megaparsec.ca>
2023-02-25 09:56:16 -0700

    Turn this into a flake for Typescript

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "This is a program representing rational numbers in Python";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in
+    {
+      devShell.${system} =
+        pkgs.mkShell
+          {
+            # The packages we need for this project
+            buildInputs = with pkgs;
+              [
+                nodejs
+                yarn
+              ];
+          };
+    };
+}
